1797fd2b29232e35493b134fbe1b1f6d5b427ea3
abr-ya <gamer05@list.ru>
2023-03-24 08:50:14 +0300

    feature: Update Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,11 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero />
+      <Hero
+        title="Photo Site Title"
+        text="Здесь небольшой текст про то, как хорошо болтаться по миру и фотографировать!.."
+        buttonLabel="clickMe"
+      />
     </>
   );
 };
