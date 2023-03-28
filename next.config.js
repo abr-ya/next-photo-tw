5f85bb00ffc3505638ae1211bba8c3fb1098b44a
abr-ya <gamer05@list.ru>
2023-03-28 08:18:14 +0300

    feature: Add Next Image to Slider component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "cdn.sanity.io"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
+import Image from "next/image";
 import { ISlaid } from "@/interfaces";
-import React from "react";
 
 interface ISlaider {
   data: ISlaid[];
 }
 
 const Slider = ({ data }: ISlaider) => {
+  const [current] = useState(0);
+
   return (
     <div id="gallery" className="max-w-[1240px] mx-auto">
       <h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
-      <div className="justify-center p-4">
+      <div className="relative flex justify-center p-4">
         {data.map((slide, index) => (
-          <img key={index} src={slide.image} alt="" />
+          <div key={index} className={index === current ? "opacity-[1] ease-in duration-1000" : "opacity-0"}>
+            {index === current && <Image src={slide.image} alt="/" width="1440" height="600" objectFit="cover" />}
+          </div>
         ))}
       </div>
     </div>
